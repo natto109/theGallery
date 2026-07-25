d1f12a9941bbb01bac59bb2a352616bbfdebd59b
natto109 <48425596+natto109@users.noreply.github.com>
2026-07-25 12:56:26 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+window.firebaseConfig = {
+    apiKey: "AIzaSyBIxiCuGXy1nIqOZ94dkgRzxiSvsI6w5z4",
+    authDomain: "the-gallery-project-fec55.firebaseapp.com",
+    databaseURL: "https://the-gallery-project-fec55-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "the-gallery-project-fec55",
+    storageBucket: "the-gallery-project-fec55.firebasestorage.app",
+    messagingSenderId: "231961752723",
+    appId: "1:231961752723:web:f1f29657776abd04f48966"
+};
